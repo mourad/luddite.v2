6a1ef94d98dd650651c8261c36d34390c131ecc4
David Joyner <david@joynerhome.net>
2018-03-29 15:17:34 -0700

    Don't insist on content negotation
    * Luddite has a design flaw where the list of accepted response formats is
      essentially fixed. This means that if/when resource handlers want to do
      something special like handling `Accept: text/csv`, then cannot do so.
    
    * As described in the comment block, this change gives handlers a way to perform
      this special handling. If content negotation fails and the handler doesn't
      override, then the response will still contain a 406 via a fallback code path.

--- negotiator_test.go
+++ negotiator_test.go
@@ -11,9 +11,44 @@ func TestDefaultContentType(t *testing.T) {
 	rw := httptest.NewRecorder()
 
 	n := newNegotiatorHandler([]string{ContentTypeJson, ContentTypeXml})
-
 	n.ServeHTTP(rw, req)
+
+	if res := rw.Result(); res != nil && res.StatusCode != http.StatusOK {
+		t.Error("result was written")
+	}
 	if rw.Header().Get(HeaderContentType) != ContentTypeJson {
 		t.Error("default content type not negotiated")
+	}
+}
+
+func TestSupportedContentType(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set(HeaderAccept, ContentTypeJson)
+	rw := httptest.NewRecorder()
+
+	n := newNegotiatorHandler([]string{ContentTypeJson, ContentTypeXml})
+	n.ServeHTTP(rw, req)
+
+	if res := rw.Result(); res != nil && res.StatusCode != http.StatusOK {
+		t.Error("result was written")
+	}
+	if ct := rw.Header().Get(HeaderContentType); ct != ContentTypeJson {
+		t.Errorf("incorrrect content type negotiated: %s", ct)
+	}
+}
+
+func TestUnsupportedContentType(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set(HeaderAccept, ContentTypeCsv)
+	rw := httptest.NewRecorder()
+
+	n := newNegotiatorHandler([]string{ContentTypeJson, ContentTypeXml})
+	n.ServeHTTP(rw, req)
+
+	if res := rw.Result(); res != nil && res.StatusCode != http.StatusOK {
+		t.Error("result was written")
+	}
+	if ct, ok := rw.Header()[HeaderContentType]; ok {
+		t.Errorf("incorrect content type negotiated: %s", ct)
 	}
 }
