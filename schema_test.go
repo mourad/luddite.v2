6e5804e0cead8cb257813a008d5cd6f4c64de8fb
Chaitanya Dandugula <chaitanya.dandugula@spirent.com>
2019-07-15 09:03:41 +0530

    Positve unit test cases for schema.go.

--- schema_test.go
+++ schema_test.go
@@ -7,7 +7,64 @@ import (
 	"testing"
 
 	"github.com/dimfeld/httptreemux"
+	"golang.org/x/tools/godoc/vfs/httpfs"
+	"golang.org/x/tools/godoc/vfs/mapfs"
 )
+
+const (
+	sampleYamlBody = "-id: \"1234\"\nname:\"dave\"\nflag:\"true\"\ndata:\"SGVsbG8gd29ybGQ=\"\ntimestamp:\"2015-03-18T14:30:00Z\""
+)
+
+func TestSchemaHandlerDefaultContentType(t *testing.T) {
+	fakeFS := httpfs.New(mapfs.New(map[string]string{
+		"v1/schema.json": sampleJsonBody,
+	}))
+	v := make(map[string]string)
+	v["version"] = "v1"
+	v["filepath"] = "schema.json"
+
+	ctx := httptreemux.AddParamsToContext(context.Background(), v)
+	req, _ := http.NewRequest("GET", "/", nil)
+	req = req.WithContext(ctx)
+	rw := httptest.NewRecorder()
+	rw.Header().Set(HeaderContentType, ContentTypeJson)
+
+	s := newSchemaHandler(fakeFS)
+	s.ServeHTTP(rw, req)
+	if ct := rw.Header().Get(HeaderContentType); ct != ContentTypeJson {
+		t.Errorf("incorrrect content type negotiated: %s", ct)
+	}
+
+	if body := string(rw.Body.String()); body != sampleJsonBody {
+		t.Errorf("JSON serialization failed, got: %s, expected: %s\n", body, sampleJsonBody)
+	}
+}
+
+func TestSchemaHandlerOctetStreamContentType(t *testing.T) {
+	fakeFS := httpfs.New(mapfs.New(map[string]string{
+		"v1/schema.yml": sampleYamlBody,
+	}))
+	v := make(map[string]string)
+	v["version"] = "v1"
+	v["filepath"] = "schema.yml"
+
+	ctx := httptreemux.AddParamsToContext(context.Background(), v)
+	req, _ := http.NewRequest("GET", "/", nil)
+	req = req.WithContext(ctx)
+	rw := httptest.NewRecorder()
+	rw.Header().Set(HeaderContentType, ContentTypeJson)
+
+	s := newSchemaHandler(fakeFS)
+	s.ServeHTTP(rw, req)
+
+	if ct := rw.Header().Get(HeaderContentType); ct != ContentTypeOctetStream {
+		t.Errorf("incorrrect content type negotiated: %s", ct)
+	}
+
+	if body := string(rw.Body.String()); body != sampleYamlBody {
+		t.Errorf("YAML serialization failed, got: %s, expected: %s\n", body, sampleYamlBody)
+	}
+}
 
 func TestSchemaHandlerGivenInvalidVersionStringLength(t *testing.T) {
 	v := make(map[string]string)
